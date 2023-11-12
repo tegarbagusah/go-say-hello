ccf4fa701a078c7e6d0d245e42b54893f5b807a5
tegarbagusah <tegarbagusah@gmail.com>
2023-11-12 20:27:12 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
-module github.com/tegarbagusah/go-say-hello
+//module github.com/tegarbagusah/go-say-hello/
+module github.com/tegarbagusah/go-say-hello/v2
 
 go 1.21.3
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,12 @@
 package go_say_hello
 
+/* v1
 func SayHello() string {
 	return "Hello World"
+}
+*/
+
+//v2
+func SayHello(name string) string {
+	return "Hello World" + name
 }
